d883661cb457f9c63c4ee1d87d37b8214152383e
victor toledo <querynetworking@gmail.com>
2023-06-28 20:13:25 -0300

    validacion de email en login

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,6 +28,9 @@ const crearUsuario = async (req, res = response) => {
 
       // Guardar usuario en db
       await usuario.save();
+
+      // Generar JWT
+      
    
       // Mostrar resultado 
        res.status(201).json({
@@ -47,16 +50,46 @@ const crearUsuario = async (req, res = response) => {
 }
 
 // Metodo login usuario
- const loginUsuario = (req, res = response) => {
+ const loginUsuario = async (req, res = response) => {
 
    const { email, password } = req.body;
 
-   res.status(201).json({
-       ok:true,
-       msg: 'Login',
-       email,
-       password
-    });
+   try {
+
+      let usuario = await Usuario.findOne({ email });
+
+      if ( !usuario ) {
+         return res.status(400).json({
+            ok: false,
+            msg: 'El usuario no existe con este email'
+         });
+      }
+
+      // Confirmar los passwords
+      const validPassword = bcrypt.compareSync( password, usuario.password )
+
+      if ( !validPassword ) {
+         return res.status(400).json({
+            ok: false,
+            msg: 'Password incorrecto'
+         });
+      }
+
+      // Generar nuestro JWT
+
+      res.json({
+         ok: true,
+         uid: usuario.id,
+         name: usuario.name
+      });
+      
+   } catch (error) {
+      console.log(error)
+      res.status(500).json({
+         ok: false,
+         msg: 'Por favor hable con el administrador'
+      })
+   }
 }
 
 // Metodo revalidar token
