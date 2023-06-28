0b0f2889790da9b5e8a13184f2367d0ea0d44961
victor toledo <querynetworking@gmail.com>
2023-06-28 01:51:37 -0300

    creacion de un usuario en base de datos y validacion del mismo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,43 @@
 // Importaciones
 const { response } = require('express');
+const Usuario = require('../models/User');
 
 
 // Metodo crear Usuario
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
 
-   const { name, email, password } = req.body;
+   const { email, password } = req.body;
 
-    res.status(201).json({
-       ok:true,
-       msg: 'Registro',
-       name,
-       email,
-       password
-    });
+   try {
+
+      let usuario = await Usuario.findOne({ email });
+
+      if ( usuario ) {
+         return res.status(400).json({
+            ok: false,
+            msg: 'Un usuario existe con correo'
+         });
+      }
+
+      usuario = new Usuario( req.body );
+   
+      await usuario.save();
+   
+   
+       res.status(201).json({
+          ok:true,
+          uid: usuario.id,
+          name: usuario.name
+       });
+      
+   } catch (error) {
+      console.log(error)
+      res.status(500).json({
+         ok: false,
+         msg: 'Por favor hable con el administrador'
+      })
+   }
+
 }
 
 // Metodo login usuario
